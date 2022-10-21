825235d6855f06e3f18738091a8b15291ccc5e6e
ive-m <ivesita.maria@gmail.com>
2022-10-21 04:39:06 -0400

    Refactoring timer

--- timer1.js
+++ timer1.js
@@ -8,13 +8,14 @@ args = args.sort(function(a, b) {
 
 let newArgs = [];
 for (const iterator of args) {
-  newArgs.push(Number(iterator));
+  newArgs.push(Number(iterator)*1000);
 }
 
 
 for (const iterator of newArgs) {
   if (typeof(iterator) === "number" && iterator > -1) {
     setTimeout(() => {
+      process.stdout.write('.');
       process.stdout.write('\x07');
 
     }, iterator);
